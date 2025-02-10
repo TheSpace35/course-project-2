be951e920134ed02cf2cbefe5ce8e08d10baf626
= <=>
2025-02-10 20:09:54 +0300

    локальное обновление (смотри дату и время коммита)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->name('index');
+
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::get('/admin', function () {
+        return view('admin');
+    })->name('admin');
+
+    Route::get('/client', function () {
+        return view('client');
+    })->name('client');
+    
+});
+
+
